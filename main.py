686e0fc890d07cd904a8521c94f71631f6cb9d0f
p.dibrovenko <dibrwork@gmail.com>
2024-01-14 16:54:08 +0300

    req added

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
+import numpy
+import requests
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
